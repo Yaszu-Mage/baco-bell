6f9be3468be6b0ed03196de859f5522788f88f42
Yaszu-Mage <76853160+Yaszu-Mage@users.noreply.github.com>
2025-04-05 01:10:05 +0000

    Deploying to gh-pages from @ Yaszu-Mage/baco-bell@a539f6fe03ea5fa9b3a1f4d77845b88d3c80913e 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743552936|21969062';
+const CACHE_VERSION = '1743815384|22453817';
 /** @type {string} */
 const CACHE_PREFIX = 'baco-bell-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
